045149b71587cd5d9b62952e925da491171c82d2
lexa62 <lexa62@tut.by>
2013-03-16 01:27:55 +0300

    normal variables names

--- main.c
+++ main.c
@@ -1,47 +1,47 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <getopt.h>
 #include <malloc.h>
 #include <string.h>
 #define all_string_maxsize 30
 #define child_num 5
 
-struct st1 {
+struct home_cure {
     char surname_distr_doc[all_string_maxsize];
 };
-struct st2 {
+struct hosp_cure {
     char hosp_num[all_string_maxsize];
     char hosp_address[all_string_maxsize];
     char surname_hosp_doc[all_string_maxsize];
 };
 struct info {
-    char child_surname[all_string_maxsize];     // variables names later
+    char child_surname[all_string_maxsize];
     int hospital_cure;
     char last_illness[all_string_maxsize];
-    union {
-        struct st1 was_not;
-        struct st2 was;
-    } x;
+    union patient_or_no {
+        struct home_cure home;
+        struct hosp_cure hospital;
+    } ch_union;
 };
 void print_alphabet(struct info *child, char *illness)
 {
     int i, j;
+    printf("\nPatients with %s:\n", illness);
     for (i = 'A'; i <= 'Z'; i++)
         for (j = 0; j < child_num; j++)
             if (!strcmp(illness, child[j].last_illness)
                 && child[j].child_surname[0] == i) {
                 if (child[j].hospital_cure)
                     printf
-                        ("\n%s hospital #%s address: %s doc: %s, illness:%s\n",
+                        ("%s, hospital #%s, address: %s, doctor: %s, illness: %s\n",
                          child[j].child_surname,
-                         child[j].x.was.hosp_num,
-                         child[j].x.was.hosp_address,
-                         child[j].x.was.surname_hosp_doc,
+                         child[j].ch_union.hospital.hosp_num,
+                         child[j].ch_union.hospital.hosp_address,
+                         child[j].ch_union.hospital.surname_hosp_doc,
                          child[j].last_illness);
                 else
-                    printf("%s , Doc:%s illness:%s\n",
+                    printf("%s, doctor: %s, illness: %s\n",
                            child[j].child_surname,
-                           child[j].x.was_not.surname_distr_doc,
+                           child[j].ch_union.home.surname_distr_doc,
                            child[j].last_illness);
             }
 }
@@ -49,48 +49,58 @@ void print_alphabet(struct info *child, char *illness)
 void database_print(struct info *child)
 {
     int i;
+    puts("Database:");
     for (i = 0; i < child_num; i++)
         if (child[i].hospital_cure)
             printf
-                ("%d: %s was in hospital #%s with address %s, Doctor:%s, illness:%s\n",
-                 i, child[i].child_surname, child[i].x.was.hosp_num,
-                 child[i].x.was.hosp_address,
-                 child[i].x.was.surname_hosp_doc, child[i].last_illness);
+                ("%d: %s, hospital #%s, address: %s, doctor: %s, illness: %s\n",
+                 i, child[i].child_surname,
+                 child[i].ch_union.hospital.hosp_num,
+                 child[i].ch_union.hospital.hosp_address,
+                 child[i].ch_union.hospital.surname_hosp_doc,
+                 child[i].last_illness);
         else
-            printf("%d: %s was not in hospital, Doc:%s illness:%s\n", i,
+            printf("%d: %s, district doctor: %s, illness: %s\n", i,
                    child[i].child_surname,
-                   child[i].x.was_not.surname_distr_doc,
+                   child[i].ch_union.home.surname_distr_doc,
                    child[i].last_illness);
 }
 
 void init_database(struct info *child)
 {
     child[0] = (struct info) {
-    .last_illness = "fg",.child_surname = "Alehin",.hospital_cure = 1};
-    child[0].x.was = (struct st2) {
-    .hosp_num = "hg",.hosp_address = "hg",.surname_hosp_doc = "hg"};
+    .last_illness = "anemia",.child_surname = "Alehin",.hospital_cure =
+            1};
+    child[0].ch_union.hospital = (struct hosp_cure) {
+    .hosp_num = "12",.hosp_address =
+            "Kalinowski street",.surname_hosp_doc = "Bobr"};
 
     child[1] = (struct info) {
-    .last_illness = "ospa",.child_surname = "Ivanov",.hospital_cure = 0};
-    child[1].x.was_not = (struct st1) {
-    .surname_distr_doc = "hg"};
+    .last_illness = "pneumonia",.child_surname =
+            "Ivanov",.hospital_cure = 0};
+    child[1].ch_union.home = (struct home_cure) {
+    .surname_distr_doc = "Kashliak"};
 
     child[2] = (struct info) {
-    .last_illness = "fg",.child_surname = "Smith",.hospital_cure = 1};
-    child[2].x.was = (struct st2) {
-    .hosp_num = "hg",.hosp_address = "hg",.surname_hosp_doc = "hg"};
+    .last_illness = "tetanus",.child_surname =
+            "Sidorov",.hospital_cure = 1};
+    child[2].ch_union.hospital = (struct hosp_cure) {
+    .hosp_num = "5",.hosp_address =
+            "Zapadnaya street",.surname_hosp_doc = "Maksimov"};
 
     child[3] = (struct info) {
-    .last_illness = "fg",.child_surname = "Petrov",.hospital_cure = 0};
-    child[3].x.was_not = (struct st1) {
-    .surname_distr_doc = "hg"};
+    .last_illness = "mumps",.child_surname = "Petrov",.hospital_cure =
+            0};
+    child[3].ch_union.home = (struct home_cure) {
+    .surname_distr_doc = "Maksimova"};
 
     child[4] = (struct info) {
-    .last_illness = "ospa",.child_surname = "Arib",.hospital_cure = 1};
-    child[4].x.was = (struct st2) {
-    .hosp_num = "hg",.hosp_address = "hg",.surname_hosp_doc = "hg"};
+    .last_illness = "anemia",.child_surname = "Frynse",.hospital_cure =
+            1};
+    child[4].ch_union.hospital = (struct hosp_cure) {
+    .hosp_num = "4",.hosp_address = "West street",.surname_hosp_doc =
+            "Melnova"};
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -98,28 +108,24 @@ int main(int argc, char *argv[])
     int opt = 0;
     init_database(child);
     opt = getopt(argc, argv, "pl:h");
-    while( opt != -1 ) {
-        switch( opt ) {
-            case 'p':
-                database_print(child);
-                break;
-                
-            case 'l':
-				print_alphabet(child, optarg);
-                break;
-                
-            case 'h':  // later
-            
-				break;
-                
-            default:
-				break;
+    while (opt != -1) {
+        switch (opt) {
+        case 'p':
+            database_print(child);
+            break;
+
+        case 'l':
+            print_alphabet(child, optarg);
+            break;
+
+        case 'h':              /* later */
+
+            break;
+
+        default:
+            break;
         }
-        
         opt = getopt(argc, argv, "pl:h");
     }
-    
-    
-    
     return 0;
 }
